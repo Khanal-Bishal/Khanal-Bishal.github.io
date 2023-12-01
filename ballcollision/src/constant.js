93b4e5bc1e598ae8dddfae5b035016df04a3d7b2
Bishal khanal <Bishaldada777@gmail.com>
2023-12-01 08:43:38 +0545

    Viewport adjusted with relative unit

--- ballcollision/src/constant.js
+++ ballcollision/src/constant.js
@@ -1,9 +1,9 @@
 const BALL_WIDTH = 25;
 const BALL_HEIGHT = 25;
 
-const VIEWPORT_HEIGHT = 500;
-const VIEWPORT_WIDTH = 1000;
-
+const VIEWPORT_WIDTH = document.querySelector(".viewport").clientWidth;
+const VIEWPORT_HEIGHT = document.querySelector(".viewport").clientHeight;
+console.log(VIEWPORT_HEIGHT);
 const SPEED = 2;
 const BALL_COUNT = 100;
 let RADIUS = BALL_WIDTH / 2;
