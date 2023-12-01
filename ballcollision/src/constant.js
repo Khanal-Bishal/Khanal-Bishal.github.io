2cb29a59c205ca52b19e67fa0769f68434b5f301
Bishal khanal <Bishaldada777@gmail.com>
2023-12-01 08:54:59 +0545

    Radius adjusted

--- ballcollision/src/constant.js
+++ ballcollision/src/constant.js
@@ -1,9 +1,9 @@
-const BALL_WIDTH = 25;
-const BALL_HEIGHT = 25;
+const BALL_WIDTH = 15;
+const BALL_HEIGHT = 15;
 
 const VIEWPORT_WIDTH = document.querySelector(".viewport").clientWidth - 20;
 const VIEWPORT_HEIGHT = document.querySelector(".viewport").clientHeight - 20;
-console.log(VIEWPORT_HEIGHT);
+
 const SPEED = 2;
 const BALL_COUNT = 100;
 let RADIUS = BALL_WIDTH / 2;
