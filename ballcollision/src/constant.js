33e372ece6a52f834fa695f73cbe8fb05a4fae96
Bishal khanal <Bishaldada777@gmail.com>
2023-12-01 08:50:27 +0545

    Re-adjusted viewport width and height

--- ballcollision/src/constant.js
+++ ballcollision/src/constant.js
@@ -1,8 +1,8 @@
 const BALL_WIDTH = 25;
 const BALL_HEIGHT = 25;
 
-const VIEWPORT_WIDTH = document.querySelector(".viewport").clientWidth;
-const VIEWPORT_HEIGHT = document.querySelector(".viewport").clientHeight;
+const VIEWPORT_WIDTH = document.querySelector(".viewport").clientWidth - 20;
+const VIEWPORT_HEIGHT = document.querySelector(".viewport").clientHeight - 20;
 console.log(VIEWPORT_HEIGHT);
 const SPEED = 2;
 const BALL_COUNT = 100;
